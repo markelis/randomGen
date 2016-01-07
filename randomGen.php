6121e6ac58f5e60993e8ab4da8576e85837e0b12
iraklis <iraklis@iraklis-VirtualBox>
2016-01-07 11:31:41 +0200

    Changed unnecessary public functions to private.

--- randomGen.php
+++ randomGen.php
@@ -15,19 +15,19 @@ class RandomGen {
     // private $numbers = array('1','2','3','4','5','6','7','8','9','0');
 
     // Generate a trully random number between 0 and $max
-    public function GenerateNum ($max){
+    private function GenerateNum ($max){
         srand(time());
         return mt_rand(0,$max);    
     }
     
     // Peak a char from my array ($element)
-    public function ReturnChar($element) {
+    private function ReturnChar($element) {
         $randNum = $this->GenerateNum(count($element) - 1);
         return $element[$randNum];
     }
     
     // Randomly choose what the next character will be
-    public function ChooseElement($choice) {
+    private function ChooseElement($choice) {
         
         switch ($choice) {
         case 0:
