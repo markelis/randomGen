0cc10549d4f823481e309bf0d35e74d79c3dba5c
iraklis <iraklis@iraklis-VirtualBox>
2016-01-07 06:37:18 +0200

    Commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+require_once 'randomGen.php';
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$myGenerator = new RandomGen();
+$CouponLength = filter_input(INPUT_POST, 'CouponLength');
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+            <title>This is a testing random engine</title> 
+    </head>
+    <body>
+        <form method="post" action="index.php">
+            <input type="text" name="CouponLength" value="<?php echo $CouponLength; ?>">
+            <input type="submit" name="submit" value="Click to Generate">
+        </form>
+        <p>This is what the button returns. A random set of characters. Like a coupon.</p>
+        <p><?php 
+        if(filter_has_var(INPUT_POST, 'CouponLength')) {
+            echo $myGenerator->UseGenerator($CouponLength); 
+        }
+        ?></p>
+    </body>
+</html>
